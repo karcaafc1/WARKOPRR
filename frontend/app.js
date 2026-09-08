fa73f4cd5ecaef31cef8c02d8661b98719e0a69d
karcaafc1 <karca.afc1@gmail.com>
2026-09-08 14:47:54 +0800

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbyuw-W4MAjdVw05Heu1gwp3FPKk7fOOQ4QBmbHGpsFXFxfuqsz8NlthulaqPwAWZ7fWJw/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbzzdd1RzVj3a_oenJrsIDPWR7NT5FP2WqUzXH__K_mss3_XeokPV1HvZIYkmxCr1EDH/exec';
 
 let currentUser = null;
 let menuCatalog = [];
